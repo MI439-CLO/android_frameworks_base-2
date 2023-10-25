AOSPA/android_frameworks_base-old

base: Add gestures control [1/2]
* Database policy
* KeyHandler for gestures inputs
* Input policy integration
* Supported ALL QCOM chipsets synaptic gestures

Relies on our AOSPA PowerHAL

Squashed:

Revert "SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod"

Reason for revert :- conflicts with our gesture changes.

Vishalcj17 :- Enable DT2W by default on doze.

Change-Id: I3ec27f7b3198fac87ed6d5b27d2857a07d0a6c58

Co-authored-by: Alexander Koskovich <zvnexus@outlook.com>
Co-authored-by: 33bca <fabian.leutenegger@bluewin.ch>
Co-authored-by: Willi Ye <williye97@gmail.com>
Co-authored-by: TheStrix <parthbhatia98@gmail.com>
Change-Id: I84aa3bcb6dc5f8b003423b5a5af3ec4e43578fde
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -596,6 +596,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
+    // Gesture key handler.
+    private KeyHandler mKeyHandler;
+
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
@@ -1975,6 +1978,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         initKeyCombinationRules();
         initSingleKeyGestureRules();
         mSideFpsEventHandler = new SideFpsEventHandler(mContext, mHandler, mPowerManager);
+        boolean enableKeyHandler = context.getResources().
+                getBoolean(com.android.internal.R.bool.config_enableKeyHandler);
+        if (enableKeyHandler) {
+            mKeyHandler = new KeyHandler(mContext);
+        }
     }
 
     private void initKeyCombinationRules() {
@@ -3619,6 +3627,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
 
+        /**
+         * Handle gestures input earlier then anything when screen is off.
+         */
+        if (!interactive) {
+            if (mKeyHandler != null && mKeyHandler.handleKeyEvent(event)) {
+                return 0;
+            }
+        }
+
         // Basic policy based on interactive state.
         int result;
         if (interactive || (isInjected && !isWakeKey)) {
@@ -3891,7 +3908,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             case KeyEvent.KEYCODE_WAKEUP: {
                 result &= ~ACTION_PASS_TO_USER;
-                isWakeKey = true;
+                isWakeKey = false;  // We handle this in KeyHandler
                 break;
             }
 
@@ -5004,6 +5021,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
         mGestureLauncherService = LocalServices.getService(GestureLauncherService.class);
+        if (mKeyHandler != null) {
+            mKeyHandler.systemReady();
+        }
     }
 
     /** {@inheritDoc} */

MI439-CLO/android_frameworks_base-2

Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -651,6 +651,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
+    // Gesture key handler.
+    private KeyHandler mKeyHandler;
+
     // Fallback actions by key code.
     private final SparseArray<KeyCharacterMap.FallbackAction> mFallbackActions =
             new SparseArray<KeyCharacterMap.FallbackAction>();
@@ -2354,6 +2357,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         initKeyCombinationRules();
         initSingleKeyGestureRules();
         mSideFpsEventHandler = new SideFpsEventHandler(mContext, mHandler, mPowerManager);
+        boolean enableKeyHandler = mContext.getResources().
+                getBoolean(com.android.internal.R.bool.config_enableKeyHandler);
+        if (enableKeyHandler) {
+            mKeyHandler = new KeyHandler(mContext);
+        }
     }
 
     private void initKeyCombinationRules() {
@@ -4163,6 +4171,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     + " policyFlags=" + Integer.toHexString(policyFlags));
         }
 
+        /**
+         * Handle gestures input earlier then anything when screen is off.
+         */
+        if (!interactive) {
+            if (mKeyHandler != null && mKeyHandler.handleKeyEvent(event)) {
+                return 0;
+            }
+        }
+
         // Basic policy based on interactive state.
         int result;
         if (interactive || (isInjected && !isWakeKey)) {
@@ -4435,7 +4452,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             case KeyEvent.KEYCODE_WAKEUP: {
                 result &= ~ACTION_PASS_TO_USER;
-                isWakeKey = true;
+                isWakeKey = false;  // We handle this in KeyHandler
                 break;
             }
 
@@ -5525,6 +5542,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mAutofillManagerInternal = LocalServices.getService(AutofillManagerInternal.class);
         mGestureLauncherService = LocalServices.getService(GestureLauncherService.class);
+        if (mKeyHandler != null) {
+            mKeyHandler.systemReady();
+        }
     }
 
     /** {@inheritDoc} */